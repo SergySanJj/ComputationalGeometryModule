b49ac64503c40f76c9a30cdb044a68c51618c42a
Sergei <isara.isara8@gmail.com>
2020-05-06 12:35:28 +0300

    Add project for 3d nurbs

---
+++ 3dNURBS/scripts/__main__.py
@@ -0,0 +1,9 @@
+from scripts.nurbs.nurbs import get_NURBS_points
+
+
+def main():
+    print("hello", get_NURBS_points(["a", "b"]))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 3dNURBS/scripts/nurbs/nurbs.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def get_NURBS_points(control_points: List):
+    return control_points
